ccae50140a05d57654202e20a8140a8ae409a811
AdarshveerSingh <adarshveer3001.beai24@chitkara.edu.in>
2026-08-08 12:59:06 +0530

    Added Approach to ClinicAndVillage code

--- Week5/ClinicAndVillage.c
+++ Week5/ClinicAndVillage.c
@@ -1,3 +1,16 @@
+/*
+    APPROACH
+
+The idea is to guess the maximum number of patients a clinic can handle and check whether that guess is possible 
+with the available k clinics. We start with a range from 0 to the largest village population and repeatedly take the 
+middle value. For each village, we calculate how many clinics it would need so that each clinic has at most that guessed load.
+ If the total number of required clinics is ≤ k, the guess works, so we try an even smaller load. If it needs more than k, 
+ the guess is too small, so we increase it. We keep narrowing this range until the difference is extremely small, 
+ and the resulting value is the minimum possible maximum clinic load.
+ 
+ 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
